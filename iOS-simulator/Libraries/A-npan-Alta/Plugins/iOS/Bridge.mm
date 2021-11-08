102588f5e5c9f9463de19c5becb6c1a9646a2d70
Toru <toru.inoue@kissaki.tv>
2021-11-08 15:45:23 +0900

    間違えて別ディレクトリのものを加えていた

--- iOS-simulator/Libraries/A-npan-Alta/Plugins/iOS/Bridge.mm
+++
@@ -1,7 +0,0 @@
-#import <UnityFramework/UnityFramework-Swift.h>
-
-extern "C" {
-    void initialize(const char *code) {
-        [AltaHeadEngine initialize:[NSString stringWithUTF8String:code]];
-    }
-}
